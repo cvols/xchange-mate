36374b32d7e4b1907339236b1284eb0a79ed7692
Aaron Silverstein <adsilvercomp@Aarons-MacBook-Pro.local>
2017-12-11 01:22:44 -0500

    app reciever update

--- public/js/app-reciever.js
+++ public/js/app-reciever.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     $('select').material_select();
-  });
+});
 
 console.log("inital");
 
@@ -9,31 +9,54 @@ console.log("inital");
 
 $("#submit").click(function () {
 
-     var desiredCurrency =    $("#dCurrency").val().trim();
-     var currentCurrency =    $("#cCurrency").val().trim();
-     var desiredAmount =      $("#desiredAmount").val().trim();
-     var transLocation =      $("#transLocation").val().trim();
-     var transDate =          $("#transDate").val().trim();
-     var queryUrl = "https://api.fixer.io/latest?base=" + desiredCurrency
-     var queryUrl2 = "https://api.fixer.io/latest?symbols=" +  currentCurrency
+    var desiredCurrency = $("#dCurrency").val().trim();
+    var currentCurrency = $("#cCurrency").val().trim();
+    var desiredAmount = $("#desiredAmount").val().trim();
+    var transLocation = $("#transLocation").val().trim();
+    var transDate = $("#transDate").val().trim();
+    var queryUrl = "http://api.fixer.io/latest?base=" + desiredCurrency + "&symbols=" + currentCurrency
+     
+    var pain = JSON.stringify(desiredCurrency);
 
-        console.log("desired currency" + " " + desiredCurrency);
-        console.log("current currency" + " " + currentCurrency);
-        console.log("desired amount" + " " + desiredAmount);
-        console.log("transaction location" + " " + transLocation);
-        console.log("transaction Date" + " " + transDate);
+
+    console.log("desired currency" + " " + desiredCurrency);
+    console.log("current currency" + " " + currentCurrency);
+    console.log("desired amount" + " " + desiredAmount);
+    console.log("transaction location" + " " + transLocation);
+    console.log("transaction Date" + " " + transDate);
 
     $.ajax({
-        url: queryUrl, queryUrl2,
+        url: queryUrl,
         method: 'GET',
         dataType: 'json'
     }).done(function (response) {
-       
-        console.log(response.base = desiredCurrency);
-        console.log("fee" + " " + response.rates.USD * desiredAmount + " " + currentCurrency);
-        // console.log(response.base);
-        // console.log(response.rates.GBP);
         
+        console.log("USD" + response.rates.USD);
+        var obj = response.rates
+        console.log(response.rates);
+        for (x in obj) {
+         // if (obj.hasOwnProperty(desiredCurrency)) {
+            var val = obj[x];
+            console.log(val);
+            console.log( "fee" + " " + val * desiredAmount + " " + currentCurrency);
+            // if(obj==desiredCurrency){
+            //     console.log("hello");
+            // }else{
+            //     console.log("this does not work");
+            // }
+            
+          //}
+        }
+      
+        // console.log(response.rates["USD"]);
+        //the fee = the amount of your current currency inside of a dolloar of your desired currency * your desired amount of desired currency
+
+        //Problem - how do you get the amount of your current currency inside of dollar of desired currency in a dynamic way. Right now you have to explicitly write the desired currency in dot notation to retrieve the difference.
+        // console.log("fee" + " " + response.rates.USD * desiredAmount + " " + currentCurrency);
+
+
+
+
     });
 });
 
