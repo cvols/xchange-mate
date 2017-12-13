e55b1a08acb060504d7423e665e417f61694550f
cvols <chris.volonnino@gmail.com>
2017-12-12 20:44:58 -0500

    chris v- updates

--- models/transaction.js
+++ models/transaction.js
@@ -39,7 +39,7 @@ module.exports = function (sequelize, DataTypes) {
             foreignKey: {
                 allowNull: false
             }
-        });
+        })
     }
 
     return Transaction
