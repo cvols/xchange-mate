74df0539e3e17222cc9bccd3aae635c2ddede059
cvols <chris.volonnino@gmail.com>
2017-12-15 14:03:42 -0500

    config

---
+++ config/vendorPassport.js
@@ -0,0 +1,49 @@
+var passportV = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+
+var db = require("../models");
+
+// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
+passportV.use(new LocalStrategy(
+  // Our user will sign in using an email, rather than a "username"
+  {
+    usernameField: "email"
+  },
+  function(email, password, done) {
+    // When a user tries to sign in this code runs
+    db.Vendor.findOne({
+      where: {
+        email: email
+      }
+    }).then(function(dbVendor) {
+      // If there's no user with the given email
+      if (!dbVendor) {
+        return done(null, false, {
+          message: "Incorrect email."
+        });
+      }
+      // If there is a user with the given email, but the password the user gives us is incorrect
+      else if (!dbVendor.validPassword(password)) {
+        return done(null, false, {
+          message: "Incorrect password."
+        });
+      }
+      // If none of the above, return the user
+      return done(null, dbVendor);
+    });
+  }
+));
+
+// In order to help keep authentication state across HTTP requests,
+// Sequelize needs to serialize and deserialize the user
+// Just consider this part boilerplate needed to make it all work
+passportV.serializeUser(function(vendor, cb) {
+  cb(null, vendor);
+});
+
+passportV.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+// Exporting our configured passport
+module.exports = passportV;
