a1d36cb82453f0c420449f7f3df2e894b1406834
jcastanien <jcastanien@gmail.com>
2017-12-16 04:15:40 -0500

    passportupdate

--- controllers/xchange_controller.js
+++ controllers/xchange_controller.js
@@ -2,7 +2,7 @@
 
 // import models
 var db = require('../models');
-var passport = require("../config/passport");
+// var passport = require("../config/passport");
 var passportV = require("../config/vendorPassport")
 
 
@@ -16,7 +16,7 @@ module.exports = function (app) {
         res.render('./index');
     })
 
-    app.post("/custLogin", passport.authenticate("local"), function (req, res) {
+    app.post("/custLogin", passportV.authenticate("local"), function (req, res) {
         // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
         // So we're sending the user back the route to the members page because the redirect will happen on the front end
         // They won't get this or even be able to access this page if they aren't authed
--- public/js/app-vendor.js
+++ public/js/app-vendor.js
@@ -13,11 +13,18 @@ $(function () {
         $.ajax('/api/vendor/' + id, {
             type: 'GET'
         }).then(function (data) {
+            for(var i=o; data.Transactions.length; i++) {
+                for(var j = 0; j < data.Transactions[i].id.length; j++) {
+                    console.log(data.Transactions[i].id[j])
+                    if(data.Transactions[i].id[j] == id)
+                }
+            }
+
             $('#first_name').html(data.first_name)
             $('#last_name').html(data.last_name)
             $('#phone_number').html(data.phone_number)
             $('#email').html(data.email)
-            $('#user_rating').html(data.user_rating)
+            $('#user_rating').html(data.total_money)
         })
     })
 
